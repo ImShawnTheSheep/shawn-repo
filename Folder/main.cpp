b39f7f3c5839849eaf49e10a0a3857c48c2acbe2
ImShawnTheSheep <shawnmichaeljumawan@gmail.com>
2022-08-24 19:24:23 +0800

    Update main.cpp

--- Folder/main.cpp
+++ Folder/main.cpp
@@ -1,8 +1,24 @@
 #include <iostream>
 
+void bubbleSort();
+
 int main(int argc, char const *argv[])
 {
     std::cout << "Hello world \n";
-    
+
     return 0;
+}
+
+void bubbleSort()
+{
+    int n = 10;
+
+    for(int i = 0; i < n; i++)
+    {
+        for(int j = 0; j < i; j++)
+        {
+            
+        }
+    }
+    
 }
